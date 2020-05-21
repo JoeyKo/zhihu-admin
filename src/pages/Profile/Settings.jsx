9e9aad2d8b65b153ec55b96cfc6b08d07fd0f4af
joeyko <joeyko1004@gmail.com>
2020-05-21 11:56:32 +0800

    style: profile settings layouts

--- src/pages/Profile/Settings.jsx
+++ src/pages/Profile/Settings.jsx
@@ -1,17 +1,14 @@
 import React, { useState, useEffect } from 'react'
 import moment from 'moment'
-import { Card, Form, Input, DatePicker, Radio, Button, Upload, message } from 'antd'
+import { Menu, Card, Form, Input, DatePicker, Radio, Button, Upload, message, Avatar, Row, Col } from 'antd'
 import PageLayout from '@/components/PageLayout'
 import { BASEURL } from '@/api/config'
-import { LoadingOutlined, PlusOutlined } from '@ant-design/icons'
+import { LoadingOutlined, PlusOutlined, UploadOutlined } from '@ant-design/icons'
 import axios from '@/api'
 
-const { TextArea } = Input
+import styles from './index.module.scss'
 
-const layout = {
-  labelCol: { span: 8 },
-  wrapperCol: { span: 10 },
-};
+const { TextArea } = Input
 
 function getBase64(img, callback) {
   const reader = new FileReader();
@@ -42,23 +39,12 @@ const ProfileSettings = () => {
   const [form] = Form.useForm();
 
   useEffect(() => {
-    const getProfile = async id => {
-      try {
-        const res = await axios.get('/api/user/profile')
-        if (res.status === 1) {
-          form.setFieldsValue({
-            ...res.profile,
-            birthday: res.profile.birthday ? moment(res.profile.birthday) : null
-          })
-          setImageUrl(res.profile.avatar ? BASEURL + res.profile.avatar.path : '')  
-        } else {
-          message.error(res.message)
-        }
-      } catch (err) {
-        console.log(err)
-      }
-    }
-    getProfile()
+    const user = JSON.parse(localStorage.getItem('user')) || {}
+
+    if (user.birthday) user.birthday = moment(user.birthday)
+    if (user.avatar) setImageUrl(BASEURL + user.avatar.path)
+    console.log(user)
+    form.setFieldsValue(user)
   }, [form])
 
   const onFinish = async values => {
@@ -67,6 +53,7 @@ const ProfileSettings = () => {
       const res = await axios.put('/api/user/profile', values)
       setLoading(false)
       if (res.status === 1) {
+        localStorage.setItem('user', JSON.stringify(res.profile || {}))
         message.success('提交成功！')
       } else {
         message.error(res.message)
@@ -84,57 +71,75 @@ const ProfileSettings = () => {
     }
     if (info.file.status === 'done') {
       // Get this url from response in real world.
+      setUploadLoading(false)
       getBase64(info.file.originFileObj, imageUrl =>
         setImageUrl(imageUrl),
       );
     }
   };
 
-  const uploadButton = (
-    <div>
-      {uploadLoading ? <LoadingOutlined /> : <PlusOutlined />}
-      <div className="ant-upload-text">上传头像</div>
-    </div>
-  );
-
   return (
     <PageLayout routes={[{ path: '/profile-settings', breadcrumbName: '基本设置' }]} title="基本设置">
-      <Card bordered={false}>
-        <Form {...layout} form={form} onFinish={onFinish}>
-          <Form.Item name="avatar" label="头像">
-            <Upload
-              name="avatar"
-              listType="picture-card"
-              showUploadList={false}
-              action={`${BASEURL}/api/user/upload-avatar`}
-              onChange={handleChange}
-              headers={{ Authorization: localStorage.getItem('token') }}
-              beforeUpload={beforeUpload}
+      <Card bordered={false} bodyStyle={{ padding: "16px 0" }}>
+        <Row>
+          <Col span={4}>
+            <Menu
+              style={{ borderRight: 'none' }}
+              defaultSelectedKeys={['basic']}
+              mode="inline"
             >
-              {imageUrl ? <img src={imageUrl} alt="avatar" style={{ width: '100%' }} /> : uploadButton}
-            </Upload>
-          </Form.Item>
-          <Form.Item name="username" label="用户名">
-            <Input />
-          </Form.Item>
-          <Form.Item name="gender" label="性别">
-            <Radio.Group>
-              <Radio value={0}>女</Radio>
-              <Radio value={1}>男</Radio>
-            </Radio.Group>
-          </Form.Item>
-          <Form.Item name="birthday" label="出生日期">
-            <DatePicker disabledDate={disabledDate} />
-          </Form.Item>
-          <Form.Item name="headline" label="个人简介">
-            <TextArea rows={3} />
-          </Form.Item>
-          <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 8 }}>
-            <Button loading={loading} type="primary" htmlType="submit">
-              提交
-          </Button>
-          </Form.Item>
-        </Form>
+              <Menu.Item key="basic">基本设置</Menu.Item>
+              <Menu.Item key="safe">安全设置</Menu.Item>
+            </Menu>
+          </Col>
+          <Col span={20}>
+            <Card title="基本设置" bordered={false}>
+              <Form layout="vertical" form={form} onFinish={onFinish}>
+                <Row>
+                  <Col span={5}>
+                    <Form.Item name="username" label="用户名">
+                      <Input />
+                    </Form.Item>
+                    <Form.Item name="gender" label="性别">
+                      <Radio.Group>
+                        <Radio value={0}>女</Radio>
+                        <Radio value={1}>男</Radio>
+                      </Radio.Group>
+                    </Form.Item>
+                    <Form.Item name="birthday" label="出生日期">
+                      <DatePicker disabledDate={disabledDate} />
+                    </Form.Item>
+                    <Form.Item name="headline" label="个人简介">
+                      <TextArea rows={3} />
+                    </Form.Item>
+                  </Col>
+                  <Col className={styles.avatarWrapper}>
+                  <Form.Item label="头像">
+                    <Upload
+                      name="avatar"
+                      showUploadList={false}
+                      action={`${BASEURL}/api/user/upload-avatar`}
+                      onChange={handleChange}
+                      headers={{ Authorization: localStorage.getItem('token') }}
+                      beforeUpload={beforeUpload}
+                    >
+                      <div className={styles.uploadWrapper}>
+                        <Avatar className={styles.avatar} src={imageUrl} />
+                        <Button loading={uploadLoading} icon={<UploadOutlined />}>更换头像</Button>
+                      </div>
+                    </Upload>
+                    </Form.Item>
+                  </Col>
+                </Row>
+                <Form.Item>
+                  <Button loading={loading} type="primary" htmlType="submit">
+                    更新基本信息
+                  </Button>
+                </Form.Item>
+              </Form>
+            </Card>
+          </Col>
+        </Row>
       </Card>
     </PageLayout>
   )
